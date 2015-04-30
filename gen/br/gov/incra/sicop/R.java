b8e0e7364e11bf649c3bd05f2b60f4841b43f368
Waldenilson Carneiro <wal.denilson@hotmail.com>
2015-04-30 20:11:19 -0300

    adaptacao das cores basicas do app.

--- gen/br/gov/incra/sicop/R.java
+++ gen/br/gov/incra/sicop/R.java
@@ -12,8 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int about=0x7f020000;
-        public static final int aluno_uniasselvi_top=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static final int button1=0x7f070007;
@@ -59,27 +58,28 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
-        public static final int ale_buscando_dados=0x7f040005;
-        public static final int ale_retorno_vazio=0x7f040004;
+        public static final int ale_buscando_dados=0x7f040006;
+        public static final int ale_retorno_vazio=0x7f040005;
         public static final int app_desc=0x7f040001;
+        public static final int app_description=0x7f040002;
         public static final int app_name=0x7f040000;
-        public static final int app_version=0x7f040002;
-        public static final int er_json=0x7f040006;
-        public static final int lb_codigo_fonte=0x7f040007;
-        public static final int lb_creditos=0x7f040009;
-        public static final int lb_desc_codigo_fonte=0x7f04000b;
-        public static final int lb_desc_creditos=0x7f04000a;
-        public static final int lb_desc_info=0x7f04000e;
-        public static final int lb_desc_novidades=0x7f04000c;
-        public static final int lb_info=0x7f04000d;
-        public static final int lb_novidades=0x7f040008;
-        public static final int lb_sair=0x7f040014;
-        public static final int lb_voltar=0x7f040013;
-        public static final int menu_settings=0x7f040003;
-        public static final int txt_codigo_fonte=0x7f04000f;
-        public static final int txt_creditos=0x7f040011;
-        public static final int txt_info=0x7f040010;
-        public static final int txt_novidades=0x7f040012;
+        public static final int app_version=0x7f040003;
+        public static final int er_json=0x7f040007;
+        public static final int lb_codigo_fonte=0x7f040008;
+        public static final int lb_creditos=0x7f04000a;
+        public static final int lb_desc_codigo_fonte=0x7f04000c;
+        public static final int lb_desc_creditos=0x7f04000b;
+        public static final int lb_desc_info=0x7f04000f;
+        public static final int lb_desc_novidades=0x7f04000d;
+        public static final int lb_info=0x7f04000e;
+        public static final int lb_novidades=0x7f040009;
+        public static final int lb_sair=0x7f040015;
+        public static final int lb_voltar=0x7f040014;
+        public static final int menu_settings=0x7f040004;
+        public static final int txt_codigo_fonte=0x7f040010;
+        public static final int txt_creditos=0x7f040012;
+        public static final int txt_info=0x7f040011;
+        public static final int txt_novidades=0x7f040013;
     }
     public static final class style {
         /** 
--- src/br/gov/incra/sicop/MainActivity.java
+++ src/br/gov/incra/sicop/MainActivity.java
@@ -1,17 +1,27 @@
 package br.gov.incra.sicop;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.csl.alunouniasselvi.R;
+import br.com.csl.alunouniasselvi.list.ListViewImageAdapter;
 import br.gov.incra.sicop.abstractactivity.IActivity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v4.view.ViewPager.LayoutParams;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
 
-public class MainActivity extends Activity implements IActivity {
+public class MainActivity extends Activity implements IActivity, OnItemClickListener {
 
     private ProgressDialog pd;
+    private ListView lvmenu;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,18 @@ public class MainActivity extends Activity implements IActivity {
 		setContentView(R.layout.activity_main);
 		getWindow().setLayout(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
 		init();
+		List<String> lista = new ArrayList<String>();
+		List<String> desc = new ArrayList<String>();
+		List<Drawable> img = new ArrayList<Drawable>();
+		
+		lista.add( getString(R.string.lb_simulador_notas) ); desc.add( getString( R.string.lb_desc_simulador_notas) );//simulador
+		img.add( getResources().getDrawable(R.drawable.calculator) );
+
+		ListViewImageAdapter lv = new ListViewImageAdapter(this, lista, desc, img);
+		lvmenu.setAdapter(lv);
+		lvmenu.setTextFilterEnabled(true);
+		lvmenu.setOnItemClickListener(this);	
+
 	}
 
 	private void init(){
@@ -38,6 +60,11 @@ public class MainActivity extends Activity implements IActivity {
 		
 	}
 
+
+	public void click_bt_bar_info(View v) {
+		// TODO Auto-generated method stub
+		
+	}
 	@Override
 	public void abrirDialogProcessamento() {
 		// TODO Auto-generated method stub
@@ -71,5 +98,11 @@ public class MainActivity extends Activity implements IActivity {
 	
 	public void bt_sair(View v){
 		finish();
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+		// TODO Auto-generated method stub
+		
 	}
 }
