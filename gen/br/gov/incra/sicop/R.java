067d2ef135e65cae71d8e69f2dd05bd1ec38d82e
Waldenilson Carneiro <wal.denilson@hotmail.com>
2015-04-30 17:27:58 -0300

    atribuicao das strings principais do aplicativo e criacao da main_activity.

--- gen/br/gov/incra/sicop/R.java
+++ gen/br/gov/incra/sicop/R.java
@@ -21,27 +21,28 @@ public final class R {
         public static final int et_login_senha=0x7f070006;
         public static final int imageView1=0x7f070000;
         public static final int imageView2=0x7f070002;
-        public static final int iv_listimage=0x7f070014;
-        public static final int ll_listcolor=0x7f070009;
-        public static final int ll_listdetailcolor=0x7f070010;
-        public static final int menu_settings=0x7f070019;
+        public static final int iv_listimage=0x7f070015;
+        public static final int ll_listcolor=0x7f07000a;
+        public static final int ll_listdetailcolor=0x7f070011;
+        public static final int lv_menu=0x7f070009;
+        public static final int menu_settings=0x7f07001a;
         public static final int scrollView1=0x7f070004;
-        public static final int textView1=0x7f070008;
+        public static final int textView2=0x7f070008;
         public static final int textView3=0x7f070001;
         public static final int textView4=0x7f070003;
-        public static final int tv_listcolor_desc=0x7f07000b;
-        public static final int tv_listcolor_title=0x7f07000a;
-        public static final int tv_listconfig=0x7f07000c;
-        public static final int tv_listdetail_desc=0x7f07000f;
-        public static final int tv_listdetail_title=0x7f07000d;
-        public static final int tv_listdetail_value=0x7f07000e;
-        public static final int tv_listdetailcolor_desc=0x7f070013;
-        public static final int tv_listdetailcolor_title=0x7f070011;
-        public static final int tv_listdetailcolor_value=0x7f070012;
-        public static final int tv_listimage=0x7f070015;
-        public static final int tv_listimagedesc=0x7f070016;
-        public static final int tv_listmenu=0x7f070017;
-        public static final int tv_listmenudesc=0x7f070018;
+        public static final int tv_listcolor_desc=0x7f07000c;
+        public static final int tv_listcolor_title=0x7f07000b;
+        public static final int tv_listconfig=0x7f07000d;
+        public static final int tv_listdetail_desc=0x7f070010;
+        public static final int tv_listdetail_title=0x7f07000e;
+        public static final int tv_listdetail_value=0x7f07000f;
+        public static final int tv_listdetailcolor_desc=0x7f070014;
+        public static final int tv_listdetailcolor_title=0x7f070012;
+        public static final int tv_listdetailcolor_value=0x7f070013;
+        public static final int tv_listimage=0x7f070016;
+        public static final int tv_listimagedesc=0x7f070017;
+        public static final int tv_listmenu=0x7f070018;
+        public static final int tv_listmenudesc=0x7f070019;
     }
     public static final class layout {
         public static final int activity_layout=0x7f030000;
@@ -58,10 +59,27 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
-        public static final int ale_buscando_dados=0x7f040003;
+        public static final int ale_buscando_dados=0x7f040005;
+        public static final int ale_retorno_vazio=0x7f040004;
+        public static final int app_desc=0x7f040001;
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
+        public static final int app_version=0x7f040002;
+        public static final int er_json=0x7f040006;
+        public static final int lb_codigo_fonte=0x7f040007;
+        public static final int lb_creditos=0x7f040009;
+        public static final int lb_desc_codigo_fonte=0x7f04000b;
+        public static final int lb_desc_creditos=0x7f04000a;
+        public static final int lb_desc_info=0x7f04000e;
+        public static final int lb_desc_novidades=0x7f04000c;
+        public static final int lb_info=0x7f04000d;
+        public static final int lb_novidades=0x7f040008;
+        public static final int lb_sair=0x7f040014;
+        public static final int lb_voltar=0x7f040013;
+        public static final int menu_settings=0x7f040003;
+        public static final int txt_codigo_fonte=0x7f04000f;
+        public static final int txt_creditos=0x7f040011;
+        public static final int txt_info=0x7f040010;
+        public static final int txt_novidades=0x7f040012;
     }
     public static final class style {
         /** 
